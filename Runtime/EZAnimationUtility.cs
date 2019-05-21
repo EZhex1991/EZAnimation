4091aeab9d851373bca70a4e16952cebe1889401
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2019-05-21 11:27:20 +0800

    first commit

---
+++ Runtime/EZAnimationUtility.cs
@@ -0,0 +1,47 @@
+/* Author:          ezhex1991@outlook.com
+ * CreateTime:      2018-01-24 14:53:33
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using UnityEngine;
+
+namespace EZUnity.Animation
+{
+    public static class EZAnimationUtility
+    {
+        public static Gradient GradientFadeOut()
+        {
+            GradientColorKey[] colorKeys = new GradientColorKey[]
+            {
+                new GradientColorKey(Color.white, 0),
+                new GradientColorKey(Color.black, 1),
+            };
+            GradientAlphaKey[] alphaKeys = new GradientAlphaKey[]
+            {
+                new GradientAlphaKey(1, 0),
+                new GradientAlphaKey(0, 1),
+            };
+
+            Gradient gradient = new Gradient();
+            gradient.SetKeys(colorKeys, alphaKeys);
+            return gradient;
+        }
+        public static Gradient GradientFadeIn()
+        {
+            GradientColorKey[] colorKeys = new GradientColorKey[]
+            {
+                new GradientColorKey(Color.black, 0),
+                new GradientColorKey(Color.white, 1),
+            };
+            GradientAlphaKey[] alphaKeys = new GradientAlphaKey[]
+            {
+                new GradientAlphaKey(0, 0),
+                new GradientAlphaKey(1, 1),
+            };
+
+            Gradient gradient = new Gradient();
+            gradient.SetKeys(colorKeys, alphaKeys);
+            return gradient;
+        }
+    }
+}
